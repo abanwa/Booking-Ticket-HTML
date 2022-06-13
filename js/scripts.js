58573380709bb4eb8d28d793e72e958b5ec74541
Chinaza <abanwachinaza@example.com>
2022-06-13 18:39:21 +0100

    Booking Ticket

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// let vid = document.querySelector("#myVideo > source");
+// let vidSrc = vid.getAttribute('src');
+ // console.log(vidSrc);
+// console.log(innerWidth);
+// if (innerWidth >= 414){
+// 	vid.setAttribute('src', null);
+// }
+
+
+
+
+
+
+
+
+
+
+
